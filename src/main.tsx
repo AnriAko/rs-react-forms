61a4d23de8d0596a50dcf9e9d6163049dce94b62
AnriAko <ejik.v.tu.m.ane97@gmail.com>
2025-08-28 16:34:41 +0400

    fix: add additional packages

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
-import { store } from '~/redux/store';
 
 const rootElement = document.getElementById('root');
 
@@ -11,9 +9,7 @@ if (rootElement) {
   const root = createRoot(rootElement);
   root.render(
     <StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </StrictMode>
   );
 } else {
